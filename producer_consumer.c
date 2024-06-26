ae4be224fe0047b4959bf715c91123188fac48fd
Niket Dugar <143904117+Niketjr@users.noreply.github.com>
2024-06-26 10:54:17 +0530

    Update producer_consumer.c

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,45 +1,80 @@
 #include <stdio.h>
+
 #define MAX 10
 
 int sharedMemory[MAX];
+int top = -1;
 int mutex = 1;
-int empty = MAX-1;
+int empty = MAX;
 int full = 0;
 
 void wait(int *s){
-    s--;
+    (*s)--;
 }
 
 void signal(int *s){
-    s++;
+    (*s)++;
 }
-void producer(int A[],int *m, int *e, int *f){
+
+void producer(int A[], int *top, int *m, int *e, int *f){
     if(*e != 0){
         wait(m);
-        A[f] = 1;
+        A[++(*top)] = 1; // Produce an item (here represented by 1)
         wait(e);
         signal(f);
         signal(m);
+    } else {
+        printf("Buffer is full!\n");
     }
 }
-void consumer(int A[],int *m, int *e, int *f){
+
+void consumer(int A[], int *top, int *m, int *e, int *f){
     if(*f != 0){
         wait(m);
-        A[f] = 1;
+        A[(*top)--] = 0; // Consume an item (here represented by setting it to 0)
         signal(e);
         wait(f);
-        signl(m);
+        signal(m);
+    } else {
+        printf("Buffer is empty!\n");
     }
 }
-void main(){
-    int optn;
-    printf("buffer size is 10");
-    printf("1.produce\n2.consume\n3.exit");
-    while(1){
-        printf("enter optn: ");
-        scanf("%d", &)
-        if(optn == 1){
-            producer(sharedMemory, &mutex, &empty, &full);
+
+void printBuffer(int A[], int top) {
+    printf("Buffer: ");
+    for(int i = 0; i < MAX; i++) {
+        if(i <= top) {
+            printf("%d ", A[i]);
+        } else {
+            printf("0 ");
         }
     }
+    printf("\n");
+}
+
+int main(){
+    int optn;
+    printf("Buffer size is %d\n", MAX);
+    printf("1. Produce\n2. Consume\n3. Exit\n");
+
+    while(1){
+        printf("Enter option: ");
+        scanf("%d", &optn);
+        switch(optn) {
+            case 1:
+                producer(sharedMemory, &top, &mutex, &empty, &full);
+                printBuffer(sharedMemory, top);
+                break;
+            case 2:
+                consumer(sharedMemory, &top, &mutex, &empty, &full);
+                printBuffer(sharedMemory, top);
+                break;
+            case 3:
+                return 0;
+            default:
+                printf("Invalid option!\n");
+                break;
+        }
+    }
+    return 0;
 }
